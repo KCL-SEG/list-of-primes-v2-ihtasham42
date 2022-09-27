fd265ee40005e56d5861b2dfb31562e21ff7fffa
Ihtasham <chowdhuryihtasham42@gmail.com>
2022-09-27 23:23:11 +0100

    solution

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError
+
+    res = []
+
+    i = 2
+
+    while len(res) < number_of_primes:
+        prime = True
+        for j in range(2, i - 1):
+            if i // j == i / j:
+                prime = False
+        if prime:
+            res.append(i)
+        i += 1
+        
+    return res
